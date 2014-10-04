47fd59fd1b8918e983b24aafef7bada255ded714
Jake <me@jh.gg>
2014-10-04 05:56:35 -0400

    Update geocode module to use new module opt interface :>

--- modules/geocode.py
+++ modules/geocode.py
@@ -2,10 +2,28 @@ from jeev.message import Attachment
 from jeev.utils.importing import import_dotted_path
 import module
 
+module.opt(
+    'using',
+    'The geopy geocoder (or similar interface) to use.',
+    default='geopy.geocoders.Nominatim')
+
+
+@module.opt_validator('using')
+def validate_is_geocoder(name):
+    try:
+        module = import_dotted_path(name)
+    except ImportError:
+        raise module.ConfigError("Could not import module %s" % name)
+
+    if not hasattr(module, 'geocode'):
+        raise module.ConfigError("Module %s does not have a method 'geocode'" % name)
+
+    return module
+
 
 @module.loaded
 def loaded():
-    module.g.geocoder = import_dotted_path(module.opts.get('using', 'geopy.geocoders.Nominatim'))()
+    module.g.geocoder = module.opts['using']
 
 
 @module.respond('geocode (.*)$')
